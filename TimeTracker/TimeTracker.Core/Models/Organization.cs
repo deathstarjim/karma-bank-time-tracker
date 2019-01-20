c031af160cb4249c18578028a44fcd9287cd1925
Jim Piller <jpiller@fallsdigital.com>
2019-01-20 10:36:58 -0500

    Started adding registration form
    Started adding the registration form for organizations

---
+++ TimeTracker/TimeTracker.Core/Models/Organization.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Web;
+
+namespace TimeTracker.Core.Models
+{
+    public class Organization
+    {
+        public Organization()
+        {
+            TaxExemptionFile = new byte[] { };
+        }
+
+        public Guid Id { get; set; }
+
+        [DisplayName("What is your organization's name?")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "The organization name is required.")]
+        public string Name { get; set; }
+
+        [DisplayName("What is your organziation's website?")]
+        public string WebsiteUrl { get; set; }
+
+        [DisplayName("Upload your 1023c file:")]
+        public HttpPostedFileBase PostedFile { get; set; }
+
+        public byte[] TaxExemptionFile { get; set; }
+    }
+}
