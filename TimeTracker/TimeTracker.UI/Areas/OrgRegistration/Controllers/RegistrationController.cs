597bca55dc1f18b08a9202005e3e5de1b1ed4009
Jim Piller <jpiller@fallsdigital.com>
2019-01-20 08:21:33 -0500

    Added Registration Template
    Going to use a template from HTML5-Up for the registration layout. Shocking, I know....

---
+++ TimeTracker/TimeTracker.UI/Areas/OrgRegistration/Controllers/RegistrationController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TimeTracker.UI.Areas.OrgRegistration.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        // GET: OrgRegistration/Registration
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
