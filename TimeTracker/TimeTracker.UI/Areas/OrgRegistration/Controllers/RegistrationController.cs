f45f94b0d2e06669561a6061c4063b689ac9fb8a
Jim Piller <jpiller@fallsdigital.com>
2019-01-21 08:09:04 -0500

    Added default action and more formatting
    Added some formatting and missing fields. Also added the confirmation section.

--- TimeTracker/TimeTracker.UI/Areas/OrgRegistration/Controllers/RegistrationController.cs
+++ TimeTracker/TimeTracker.UI/Areas/OrgRegistration/Controllers/RegistrationController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TimeTracker.UI.Areas.OrgRegistration.ViewModels;
 
 namespace TimeTracker.UI.Areas.OrgRegistration.Controllers
 {
@@ -11,7 +12,9 @@ namespace TimeTracker.UI.Areas.OrgRegistration.Controllers
         // GET: OrgRegistration/Registration
         public ActionResult Index()
         {
-            return View();
+            RegistrationViewModel model = new RegistrationViewModel();
+
+            return View(model);
         }
     }
 }
