0dffcb7e7bae4532716d7036a9e15e677e7142d1
Jim Piller <jpiller@fallsdigital.com>
2019-02-06 06:54:06 -0500

    Added error controller and page
    Added a global error controller and page and a global 404 page.

---
+++ TimeTracker/TimeTracker.UI/Controllers/ErrorsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TimeTracker.UI.Controllers
+{
+    public class ErrorsController : Controller
+    {
+        // GET: Errors
+        public ActionResult Index(string errorMessage = "")
+        {
+            if (!string.IsNullOrEmpty(errorMessage))
+                ViewData.Add("ErrorMessage", errorMessage);
+
+            return View("Error");
+        }
+
+        public ActionResult PageNotFound()
+        {
+            return View();
+        }
+
+    }
+}
